f092c64899cc8c2aedac716c7d5e3ac466cbf0dc
Tom Guo <gywghhb@gmail.com>
2023-05-31 20:11:28 -0400

    upload assets

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'yuHacks 2023 -- York University\'s largest hackathon',
+  title: 'yuHacks 2023',
   description: 'yuHacks(2023) is the sixth iteration of this student-led event. Our goal is to provide a fun, inclusive, and growth-focused experience that prepares participants for their future tech careers.',
 }
 
